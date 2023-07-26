d905ce3c3cdfadc2bec4aadc5b186487e351e1c0
ethan-rng <ethan.rong@gmail.com>
2023-07-26 15:52:45 -0400

    stashed changes

--- frontend/src/APICall.js
+++ frontend/src/APICall.js
@@ -1,16 +1,20 @@
-const url = "http://127.0.0.1:5000/"
+const baseUrl = "http://127.0.0.1:5000/"
 
 function getPredict(){
     let formData = new FormData();
     formData.append("link", "https://youtube.com");
 
-    fetch(url + "playlist/7GvYHdhlFMTQvwrJTxZRHv", {
+    fetch(baseUrl + "playlist/7GvYHdhlFMTQvwrJTxZRHv", {
         method:"POST",
         body: formData
     })
     .then(response => response.json())
     .then(data => console.log(data))
     .catch(error => console.error(error));
+}
+
+const getMasterResume = () => {
+    fetch(baseUrl + "playlist")
 }
 
 export default getPredict;
