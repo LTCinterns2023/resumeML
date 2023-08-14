09da8b0141f7c2368dd8f05950c2f9c63978c61c
affansaiyed <affanimam123@gmail.com>
2023-08-14 15:00:09 -0400

    summarization code

--- backend/summarizer2.py
+++ backend/summarizer2.py
@@ -1,4 +1,17 @@
-from summarizer import Summarizer
-from summarizer.sbert import SBertSummarizer
-model = SBertSummarizer('paraphrase-MiniLM-L6-v2')
-result = model(body, num_sentences=5)
+import nltk
+from sentence_transformers import SentenceTransformer, util
+from modelCNN import Model
+import numpy as np
+import LexRank
+from LexRank import degree_centrality_scores
+
+model = SentenceTransformer('all-MiniLM-L6-v2')
+
+document = Model.initializeResumes()
+
+sentences = nltk.sent_tokenize(document)
+print("Num sentences:", len(sentences))
+embeddings = model.encode(sentences, convert_to_tensor=True)
+cos_scores = util.cos_sim(embeddings, embeddings).numpy()
+centrality_scores = degree_centrality_scores(cos_scores, threshold=None)
+most_central_sentence_indices = np.argsort(-centrality_scores)
